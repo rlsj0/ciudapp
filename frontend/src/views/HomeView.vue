85c12487e5b32459caaac205f54cb54cf95ce58c
Aidanasv <a27818@svalero.com>
2025-05-16 18:42:33 +0200

    Add AppCard

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-
+import AppGrid from "../components/AppGrid.vue"
 </script>
 
 <template>
   <main>
-    
+    <v-main>
+      <AppGrid></AppGrid>
+    </v-main>
   </main>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
