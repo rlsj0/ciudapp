38f3f2fa02bb6840fc86d935aceeeabcba449e32
ceemeese <cmsalvador92@gmail.com>
2025-05-16 11:09:12 +0200

    Add config vue & sass. Add variables

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import '@/assets/styles/style.scss';
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:   `@import "./base/_variables.scss";
+                          @import "./base/_mixins.scss";
+                          @import "./base/_global.scss";`
+      }
+    }
+  },
 })
