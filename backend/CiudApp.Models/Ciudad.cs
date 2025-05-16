42159501e6d6e153ffac45a188f74e246f57420d
rlsj0 <rodrileja@gmail.com>
2025-05-16 10:22:52 +0200

    add: create Ciudad model

---
+++ backend/CiudApp.Models/Ciudad.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CiudApp.Models;
+
+public class Ciudad
+{
+    [Key]
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Pais { get; set; }
+    public int Poblacion { get; set; }
+    public bool SoftDelete { get; set; }
+    public DateTime FechaRegistro { get; set; }
+
+    public Ciudad() { }
+}
