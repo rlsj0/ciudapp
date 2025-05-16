aed4270c9382b8ef78e42e9f8b7b35bb91055050
ceemeese <cmsalvador92@gmail.com>
2025-05-16 13:03:33 +0200

    Add header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Header from './components/AppHeader.vue'
 
 </script>
 
 <template>
-  <RouterView />
+  <v-app class="d-flex flex-column min-h-screen">
+    <Header />
+    <v-main class="flex-grow-1 d-flex flex-column fill-height">
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss" scoped>
