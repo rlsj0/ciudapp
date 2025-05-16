911c31ed5d086d0eef4c1c2655db121567479b0d
rlsj0 <rodrileja@gmail.com>
2025-05-16 12:30:10 +0200

    add: create Ciudad service and repository

--- backend/CiudApp.API/Program.cs
+++ backend/CiudApp.API/Program.cs
@@ -1,4 +1,24 @@
+using Microsof.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddScoped<ICiudadService, CiudadService>();
+// builder.Services.AddScoped<ICiudadRepository, CiudadRepository>();
+
+// Aqui el tema de la BBDD
+var connectionString = builder.Configuration.GetConnectionString("ServerDB_localhost");
+
+builder.Services.AddDbContext<CiudAppContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+
+builder.Services.AddScoped<ICiudadRepository, CiudadRepository>();
+// builder.Services.AddScoped<IResenaRepository, ResenaRepository>();
+// Add Controllers
+builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -7,38 +27,29 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+// Migraciones
+
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var db = scope.ServiceProvider.GetRequiredService<CiudAppContext>();
+    db.Database.Migrate();
 }
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
